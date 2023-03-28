456c2b2cef095ec06ad30f03ec8a0ce2691f9d1f
rogseo <rogseo@gmail.com>
2023-03-27 23:45:21 -0500

    add layout

---
+++ shopbit/src/pages/categories.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { MyPage } from "../../components/common/types";
+const CategoriesPage: MyPage = () => {
+  return <div>CategoriesPage</div>;
+};
+export default CategoriesPage;
+CategoriesPage.Layout = "Main";
